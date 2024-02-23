643b72d83b35c1a69537332f65c87c0cd97348f1
Taylor Basso <taylorbasso@gmail.com>
2024-02-22 22:38:35 -0700

    Spring boot

---
+++ src/main/java/com/fleetie/FleetieApplication.java
@@ -0,0 +1,13 @@
+package com.fleetie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fleetie/FleetieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fleetie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
